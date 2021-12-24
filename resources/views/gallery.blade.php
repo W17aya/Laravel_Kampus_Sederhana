6a934e8df35940729664a17a3f4b976441df4319
Eko Wijaya <ekowijayawindows@gmail.com>
2021-12-24 19:01:16 +0700

    mulai muncul kerangka nya

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -23,19 +23,35 @@
  </nav>
 
  <div class="container text-center mt-3 p-4 bg-white">
- <h1>Data Dosen</h1>
+ <h1>Gallery</h1>
  <div class="row">
- <div class="col-sm-8 col-md-6 m-auto">
- <ol class="list-group">
- @forelse ($dosen as $val)
- <li class="list-group-item">{{$val}}</li>
- @empty
- <div class="alert alert-dark d-inline-block">Tidak ada data...</div>
- @endforelse
- </ol>
+ <div class="col-4">
+ <img src="https://source.unsplash.com/cXUOQWdRV4I/350x250"
+ class="img-thumbnail img-fluid">
+ </div>
+ <div class="col-4">
+ <img src="https://source.unsplash.com/kfwPJieZVwI/350x250"
+ class="img-thumbnail img-fluid">
+ </div>
+ <div class="col-4">
+ <img src="https://source.unsplash.com/8CqDvPuo_kI/350x250"
+ class="img-thumbnail img-fluid">
+ </div>
+ <div class="col-4 mt-4">
+ <img src="https://source.unsplash.com/JdzHrfX4l4Q/350x250"
+ class="img-thumbnail img-fluid">
+ </div>
+ <div class="col-4 mt-4">
+ <img src="https://source.unsplash.com/T-tVt4xsCdE/350x250"
+ class="img-thumbnail img-fluid">
+ </div>
+ <div class="col-4 mt-4">
+ <img src="https://source.unsplash.com/F8t2VGnI47I/350x250"
+ class="img-thumbnail img-fluid">
  </div>
  </div>
-</div>
+ </div>
+
 
  <footer class="bg-dark py-4 text-white mt-4">
 
--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -4,42 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <title>Data Mahasiswa</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
- <ul class="navbar-nav">
- <li class="nav-item">
- <a class="nav-link active" href="/mahasiswa">Data Mahasiswa</a>
- </li>
- <li class="nav-item">
- <a class="nav-link" href="/dosen">Data Dosen</a>
- </li>
- <li class="nav-item">
- <a class="nav-link" href="/gallery">Gallery</a>
- </li>
- </ul>
- </nav>
+    @include ('layouts.header')
 
- <div class="container text-center mt-3 p-4 bg-white">
- <h1>Data Mahasiswa</h1>
- <div class="row">
- <div class="col-sm-8 col-md-6 m-auto">
- <ol class="list-group">
- @forelse ($mahasiswa as $val)
- <li class="list-group-item">{{$val}}</li>
- @empty
- <div class="alert alert-dark d-inline-block">Tidak ada data...</div>
- @endforelse
- </ol>
- </div>
- </div>
-</div>
+    <div class="container text-center mt-3 p-4 bg-whiter">
+        <h1>Data Mahasiswa </h1>
+        <div class="row">
+            <div class="col-sm-8 col-md-6 m-auto">
+                <ol class="list-group">
+                    @forelse ($mahasiswa as $val)
+                    <li class="list-group-item"> {{$val}}</li>
+                    @empty
+                    <div class="alert alert-dark d-inline-block" Tidak ada data...</div>
+                @endforelse
+                </ol>
+            </div>
+        </div>
+    </div>
 
- <footer class="bg-dark py-4 text-white mt-4">
-
- </footer>
-
- </body>
- </html>
+    @include('layouts.footer')
+</body>
+</html>
